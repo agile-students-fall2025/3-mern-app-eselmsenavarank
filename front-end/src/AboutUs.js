fefede6a161849e37ffead7f915598ff33ebf29d
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-05 17:04:18 -0400

    created aboutUs and added the intro paragraph

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,14 @@
+
+const AboutUs = props => {
+
+    return(
+        <>
+        <h2>Hello! My name is Eslem.</h2>
+        <p>
+            I am majoring in CS and this is my last year at school! Previously I took Applied Internet Technologies with Prof.Versoza and I loved it. 
+            During that class we mostly focused on the technical side of bulding a software. Now I wamt to more focus of professional side of the work. 
+            Therefore I am so excited about this class. My other passion is music and I love learning how to play musical instruments. I can play around 5 different instruments now!
+        </p>
+        </>
+    )
+}
